277dd32074c4685673675a9849e5439ecf038373
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2025-04-02 11:32:39 -0300

    trabalhando com grupos

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-echo "Criando usuários do sistema..."
+echo "Criado Grupo GRP_ADM"
+groupadd GRP_ADM
 
-useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd 1234)
+echo "Criando usuários do sistema e para o grupo GRP_ADM..."
+
+useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd 1234) -G GRP_ADM
 passwd guest10 -e
 
-useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd 1234)
+useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd 1234) -G GRP_ADM
 passwd guest11 -e
 
-useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd 1234)
+useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd 1234) -G GRP_ADM
 passwd guest12 -e
 
-useradd guest13 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd 1234)
+useradd guest13 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd 1234) -G GRP_ADM
 passwd guest13 -e
 
 echo "Finalizado!!"
